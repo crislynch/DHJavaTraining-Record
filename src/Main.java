a563a3d3e59997368b67bb51a0425468f9a94e5a
Cristian Tomulescu <150183775+crislynch@users.noreply.github.com>
2024-02-19 16:48:34 +0100

    creazione programma

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Punto punto = new Punto(5, 10);
+
+        System.out.println("Coordinate del punto: (" + punto.x() + ", " + punto.y() + ")");
+    }
+}
